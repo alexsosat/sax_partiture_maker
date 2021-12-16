a30dc8d6e6ccc3e02ab571fd3d80ef5232406437
alexsosat <1080810@alumno.um.edu.mx>
2021-12-15 19:23:50 -0600

    Changes app name

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.CreateAndShow(L"sax_music_editor", origin, size)) {
+  if (!window.CreateAndShow(L"Editor de partituras Sax", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
