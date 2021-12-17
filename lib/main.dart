05a31b684c9c356ba1437017c81429dd86ced71d
alexsosat <1080810@alumno.um.edu.mx>
2021-12-17 11:40:15 -0600

    Sets windows app min and max sizes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:desktop_window/desktop_window.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sax_music_editor/routes/app_pages.dart';
+import 'dart:io' show Platform;
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    await DesktopWindow.setMinWindowSize(const Size(600, 600));
+    await DesktopWindow.setMaxWindowSize(const Size(1920, 1080));
+  }
   runApp(const MyApp());
 }
 
--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,10 +6,13 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <desktop_window/desktop_window_plugin.h>
 #include <libwinmedia/libwinmedia_plugin.h>
 #include <printing/printing_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  DesktopWindowPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("DesktopWindowPlugin"));
   LibwinmediaPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("LibwinmediaPlugin"));
   PrintingPluginRegisterWithRegistrar(
